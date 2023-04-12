a6c968f5ddd51cc56e13f0dbe5f51406c3625494
Wallace Gomes de Souza <wallace.souza@capco.com>
2023-04-12 09:56:41 -0300

    First commit

---
+++ crudsbm/src/test/java/br/com/wgs/crudsbm/CrudsbmApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.wgs.crudsbm;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CrudsbmApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
